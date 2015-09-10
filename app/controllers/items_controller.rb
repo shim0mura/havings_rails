18c4c538f0fd74842d6a9bbc9dd57fa91a9a465b
Tatsuhiko Shimomura <tatshimomura@gmail.com>
2015-09-10 22:38:53 +0900

    リストページのデザインある程度できた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -125,6 +125,7 @@ class ItemsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
+      @child_items = @item.child_items
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
